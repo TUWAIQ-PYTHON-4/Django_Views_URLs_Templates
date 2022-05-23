b01c6550f27e9c66b3333fafb57cedbaf5c0573a
RubaALmohya <ruba12399@gmail.com>
2022-05-23 11:55:23 +0300

    added new fun[movie_name] in view , added url for new fun[movie_name]

--- MoviesListApp/urls.py
+++ MoviesListApp/urls.py
@@ -2,4 +2,5 @@
 from django.urls import path
 import MoviesListApp.views
 urlpatterns = [path('', MoviesListApp.views.index, name='index'),
-              ]
+               path('Movie_name/', MoviesListApp.views.Movie_name, name='Movie_name')
+               ]
--- MoviesListApp/views.py
+++ MoviesListApp/views.py
@@ -1,7 +1,9 @@
 
 from django.shortcuts import render
 def index(request):
-        value_search= " in My page"
-        page=request.GET.get("page") or "Lab 1"
-        return render(request, "searchresult.html",{"value_search":value_search , "page":page})
+        return render(request, "base.html")
 
+def Movie_name(request):
+        value_search = " Lion King"
+        page = request.GET.get("page") or "Lab 1"
+        return render(request, "searchresult.html", {"value_search": value_search, "page": page})
